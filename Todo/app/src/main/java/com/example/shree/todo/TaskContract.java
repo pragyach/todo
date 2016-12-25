da638f213d10e4a32cfe6bcb70568b617b0c7937
pragya choudhary <pragyachoudhary759@gmail.com>
2016-12-25 16:52:02 +0530

    todo app
    android app...to make list of works which are to be done

---
+++ Todo/app/src/main/java/com/example/shree/todo/TaskContract.java
@@ -0,0 +1,19 @@
+package com.example.shree.todo;
+
+/**
+ * Created by shree on 12/25/2016.
+ */
+
+
+import android.provider.BaseColumns;
+
+public class TaskContract {
+    public static final String DB_NAME = "com.aziflaj.todolist.db";
+    public static final int DB_VERSION = 1;
+
+    public class TaskEntry implements BaseColumns {
+        public static final String TABLE = "tasks";
+
+        public static final String COL_TASK_TITLE = "title";
+    }
+}
